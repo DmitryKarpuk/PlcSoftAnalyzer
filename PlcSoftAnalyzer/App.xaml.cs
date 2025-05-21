6136cdc001277ff85844034d81e741fe2f81d0af
Dzmitry Karpuk <Dzmitry@DESKTOP-RMCVTLJ>
2025-05-21 21:23:41 +0200

    add DI registration.

--- PlcSoftAnalyzer/App.xaml.cs
+++ PlcSoftAnalyzer/App.xaml.cs
@@ -11,6 +11,7 @@ using PlcSoftAnalyzer.Services;
 using PlcSoftAnalyzer.Interfaces;
 using System.Threading;
 using PlcSoftAnalyzer.Model;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace PlcSoftAnalyzer
 {
@@ -19,6 +20,8 @@ namespace PlcSoftAnalyzer
     /// </summary>
     public partial class App : Application
     {
+        private IServiceProvider _serviceProvider;
+
         static readonly Dictionary<Model.TagAddressType, int> LimitsMap = new Dictionary<Model.TagAddressType, int>()
         {
             {TagAddressType.Input, 0 },
@@ -26,20 +29,45 @@ namespace PlcSoftAnalyzer
         };
         protected override void OnStartup(StartupEventArgs e)
         {
-            var tiaPortalService = new TiaPortalService();
-            var propgressService = new ProgressService<ProgressWindow>(new ProgressViewModel());
-            var refAnalyzerServie = new TagRefAnylizerService(LimitsMap);
-            var fileDialogService = new FileDialogService();
-            var excelReportService = new ExcelReportService();
-            var messageService = new MessageService();
-            var dataContext = new MainViewModel(propgressService, refAnalyzerServie, fileDialogService,
-                                                excelReportService, messageService, tiaPortalService);
-            MainWindow = new MainWindow()
-            {
-                DataContext = dataContext
-            };
-            MainWindow.Show();
+            //var tiaPortalService = new TiaPortalService();
+            //var progressService = new ProgressService<ProgressWindow>(new ProgressViewModel());
+            //var refAnalyzerServie = new TagRefAnylizerService(LimitsMap);
+            //var fileDialogService = new FileDialogService();
+            //var excelReportService = new ExcelReportService();
+            //var messageService = new MessageService();
+            //var dataContext = new MainViewModel(progressService, refAnalyzerServie, fileDialogService,
+            //                                    excelReportService, messageService, tiaPortalService);
+            //MainWindow = new MainWindow()
+            //{
+            //    DataContext = dataContext
+            //};
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+
+            _serviceProvider = serviceCollection.BuildServiceProvider();
+
+            var mainWindow = _serviceProvider.GetRequiredService<MainWindow>();
+            mainWindow.DataContext = _serviceProvider.GetRequiredService<MainViewModel>();
+            mainWindow.Show();
             base.OnStartup(e);
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            // DI registrations
+            services.AddSingleton<ITiaPortalService, TiaPortalService>();
+            services.AddSingleton<IProgressService, ProgressService<ProgressWindow>>();
+            services.AddSingleton<ProgressViewModel>();
+            services.AddSingleton<IFileDialogService, FileDialogService>();
+            services.AddSingleton<IExcelReportService, ExcelReportService>();
+            services.AddSingleton<IMessageService, MessageService>();
+            services.AddSingleton<ITagRefAnalyzerService, TagRefAnylizerService>(sp =>
+                new TagRefAnylizerService(LimitsMap));
+            // ViewModels
+            services.AddSingleton<MainViewModel>();
+
+            // Views
+            services.AddSingleton<MainWindow>();
         }
     }
 }
